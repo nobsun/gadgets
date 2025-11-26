d1c0fdf070cf8424f75f4fbb50d576ff52f652d9
Nobuo Yamashita <nobsun@sampou.org>
2025-11-26 17:28:01 +0900

    -s フラッグを追加

--- app/modu/Main.hs
+++ app/modu/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.List
 import Data.Bool
 import System.Environment
 import System.IO
@@ -9,18 +10,22 @@ import ModuleFile
 main :: IO ()
 main = do
     { pr <- getProgName
-    ; as <- getArgs
+    ; args <- getArgs
+    ; let spec = "-s" `elem` args
+    ; let as = delete "-s" args
     ; case as of
         [target]          -> do
             { initModule "Lib" target
             ; bool (return ())
                    (initTestModule "LibSpec" (target ++ "Spec"))
+                   . (spec &&)
                    =<< doesDirectoryExist "test"
             }
         [template,target] -> do
             { initModule template target
             ; bool (return ())
                    (initTestModule (template ++ "Spec") (target ++ "Spec"))
+                   . (spec &&)
                    =<< doesDirectoryExist "test"
             }
         _                 -> usage pr
@@ -30,7 +35,7 @@ usage :: String -> IO ()
 usage pr = hPutStrLn stderr $ unwords
     [ "Usage:"
     , pr
+    , "[-s]"
     , "[<template-module>]"
     , "<target-module>"
     ]
-
